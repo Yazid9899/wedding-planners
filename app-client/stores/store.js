ef4d45246d3c9168c5e5fa83034ef2fef98d2caa
DamarNagara04 <aragandamartimur@gmail.com>
2023-07-02 17:43:06 +0700

    feat: rtk changes

--- app-client/stores/store.js
+++ app-client/stores/store.js
@@ -4,10 +4,13 @@ import VenueReducer from "../features/VenueData/venueSlice";
 import photographReducer from "../features/PhotographData/photographSlice";
 import cateringReducer from "../features/CateringData/cateringSlice";
 
+import inputDateBudgetReducer from "../features/inputDateBudget/dateBudgetSlice";
+
 export const store = configureStore({
   reducer: {
     venue: VenueReducer,
     photograph: photographReducer,
     catering: cateringReducer,
+    inputDateBudget: inputDateBudgetReducer,
   },
 });
