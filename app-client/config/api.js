bc6c20cecf4c2fc6c32a59d5de973053b40d7bd8
ciptandaru <ciptandaru@gmail.com>
2023-07-03 09:54:24 +0700

    validasi login register

--- app-client/config/api.js
+++ app-client/config/api.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "ngrokFranky"; // ngrok franky
 //export const BASE_URL = "ngrokDamar" // ngrok damar
-export const BASE_URL = "https://d2da-103-3-221-184.ngrok-free.app"; // ngrok dhimas
+export const BASE_URL = "https://1a40-27-50-29-117.ngrok-free.app"; // ngrok dhimas
