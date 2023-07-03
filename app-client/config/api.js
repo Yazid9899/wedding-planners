f11dd5707a6af194f6b215d55c18cbba0f554c92
ciptandaru <ciptandaru@gmail.com>
2023-07-03 12:33:50 +0700

    konflik

--- app-client/config/api.js
+++ app-client/config/api.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "ngrokFranky"; // ngrok franky
-//export const BASE_URL = "ngrokDamar" // ngrok damar
+// export const BASE_URL = "https://248c-27-50-29-117.ngrok-free.app"; // ngrok damar
 export const BASE_URL = "https://90ea-27-50-29-117.ngrok-free.app"; // ngrok dhimas
