ea430208d4c0006269d185c4d34903f76769de2f
Yazid9899 <yazidsiddiq9899@gmail.com>
2023-07-03 16:52:37 +0700

    asd

--- app-client/config/api.js
+++ app-client/config/api.js
@@ -1,4 +1,6 @@
-export const BASE_URL =
-  "https://5000-2001-448a-20a0-65f4-98d8-b114-d6d7-3d0a.ngrok-free.app"; // ngrok franky
-// export const BASE_URL = "https://248c-27-50-29-117.ngrok-free.app"; // ngrok damar
+// export const BASE_URL =
+//   "https://5000-2001-448a-20a0-65f4-98d8-b114-d6d7-3d0a.ngrok-free.app"; // ngrok franky
+export const BASE_URL = "https://117d-27-50-29-117.ngrok-free.app"; // ngrok damar
 //export const BASE_URL = "ngrokDhimas" // ngrok dhimas
+// const BASE_URL = "https://9357-27-50-29-117.ngrok-free.app";
+// module.exports = BASE_URL;
