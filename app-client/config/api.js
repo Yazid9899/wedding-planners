501d145e26910c90fbff209ec5bcc299d8a65cda
Yazid9899 <yazidsiddiq9899@gmail.com>
2023-07-05 13:49:51 +0700

    jalan

--- app-client/config/api.js
+++ app-client/config/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://3e2a-27-50-29-117.ngrok-free.app"; // ngrok franky
+export const BASE_URL = "https://117d-27-50-29-117.ngrok-free.app"; // ngrok franky
 
 // export const BASE_URL = "https://067d-27-50-29-117.ngrok-free.app"; // ngrok franky
 
